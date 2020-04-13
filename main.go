81096a14ed06fc995417f17a2b1a0a6d0fa9f8b0
Jack Laxson <jackjrabbit@gmail.com>
2020-04-13 06:56:37 -0400

    work on new feature

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/spf13/viper"
@@ -14,7 +15,7 @@ import (
 
 // needs 26688 perms
 
-const Version = "0.3.0"
+const Version = "0.4.0"
 
 func ManipTxt(content string) string {
 	var out []rune
@@ -28,9 +29,39 @@ func ManipTxt(content string) string {
 	return string(out)
 }
 
+func ToClap(content string) string {
+	var out []string
+	for i, c := range strings.Split(content, " ") {
+		if i == 0 {
+			out = append(out, fmt.Sprintf("👏%s", c))
+		} else {
+			out = append(out, fmt.Sprintf("%s 👏", c))
+		}
+	}
+	return strings.Join(out, " ")
+}
+
+func DiscordSetup() {
+	const TARGET_PERMS uint = 26688
+	fmt.Println("What is the application's client_id (grab it from https://discordapp.com/developers/applications/ )?")
+	scanner := bufio.NewScanner(os.Stdin)
+	var cl_id string
+	for scanner.Scan() {
+		cl_id = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	log.Printf("connect via https://discordapp.com/oauth2/authorize?client_id=%s&scope=bot&permissions=%s", cl_id, TARGET_PERMS)
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if seq := strings.Split(m.Content, fmt.Sprintf("%ssponge", viper.GetString("prefix"))); len(seq) > 1 {
-		s.ChannelMessageSend(m.ChannelID, string(m.Author.Username)+ManipTxt(seq[1]))
+	if seq := strings.Split(m.Content, fmt.Sprintf("%ssponge ", viper.GetString("prefix"))); len(seq) > 1 {
+		s.ChannelMessageSend(m.ChannelID, string(m.Author.Username)+"» "+ManipTxt(seq[1]))
+		s.ChannelMessageDelete(m.ChannelID, m.ID)
+	}
+	if seq := strings.Split(m.Content, fmt.Sprintf("%sclap ", viper.GetString("prefix"))); len(seq) > 1 {
+		s.ChannelMessageSend(m.ChannelID, string(m.Author.Username)+"» "+ToClap(seq[1]))
 		s.ChannelMessageDelete(m.ChannelID, m.ID)
 	}
 }
@@ -39,12 +70,18 @@ func main() {
 	log.Printf("Version %v spongebob_exe bot", Version)
 	log.Println("source at: https://github.com/jrabbit/spongebob_exe")
 	viper.SetDefault("prefix", "&")
-	viper.SetConfigName("discord")
+	viper.SetConfigName("spongebob")
 	viper.AddConfigPath(".")
+	viper.AddConfigPath("/etc/")
+	viper.AddConfigPath("$HOME/.config/")
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error in config file: %s \nDo you have a config?", err))
 	}
+	if viper.GetBool("setup") {
+		return
+	}
+	log.Printf("using prefix %s", viper.GetString("prefix"))
 	discord, err := discordgo.New("Bot " + viper.GetString("token"))
 	discord.AddHandler(messageCreate)
 	discord.Open()
